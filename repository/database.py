c390b0c77feac8e1e719030834c538dd68758cc9
Junior <djalma.rsjr@gmail.com>
2021-09-09 22:29:06 -0300

    database uri

--- repository/database.py
+++ repository/database.py
@@ -1,13 +1,6 @@
 import psycopg2
 import os
 
-POSTGRES_PORT = 5432
-POSTGRES_USERNAME = "folha"
-POSTGRES_PASSWORD = "folha"
-POSTGRES_DBNAME = "journal_jj"
-POSTGRES_HOSTNAME = "172.17.0.2"
-
-
 class Database():
 
     _instances = {}
@@ -20,14 +13,9 @@ class Database():
 
     def __init__(self):
         self.connection = psycopg2.connect(
-            #host=os.getenv('DT_HOST'),
-            #user=os.getenv('DT_USER'),
-            #password=os.getenv('DT_PASS'),
-            #dbname=os.getenv('DT_NAME'),
-            #port=os.getenv('DT_PORT'))
-
-            host=POSTGRES_HOSTNAME, 
-            user=POSTGRES_USERNAME, 
-            password=POSTGRES_PASSWORD, 
-            dbname=POSTGRES_DBNAME,
-            port=POSTGRES_PORT)
+            host=os.getenv('DT_HOST'),
+            user=os.getenv('DT_USER'),
+            password=os.getenv('DT_PASS'),
+            dbname=os.getenv('DT_NAME'),
+            port=os.getenv('DT_PORT'))
+            
